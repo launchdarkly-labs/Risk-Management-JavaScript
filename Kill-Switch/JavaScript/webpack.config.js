fd234e5ea7d359297c70fd5a68f9352e16bf59c9
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2024-08-29 11:31:40 -0400

    update config file

--- Kill-Switch/JavaScript/webpack.config.js
+++ Kill-Switch/JavaScript/webpack.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   plugins: [
     new Dotenv()
-  ]
+  ],
 };
